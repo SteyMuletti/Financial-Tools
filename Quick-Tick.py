7e64138b07c13117fcbad77da3dd593022ce7a63
SteyMuletti <98870356+SteyMuletti@users.noreply.github.com>
2023-04-25 20:32:10 -0500

    Update Quick-Tick.py

--- Quick-Tick.py
+++ Quick-Tick.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import yfinance as yf
 from PyQt5.QtWidgets import QApplication, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget
+from PyQt5.QtCore import QThread, pyqtSignal
 
 class StockAnalyzer(QWidget):
     def __init__(self):
@@ -39,8 +40,19 @@ class StockAnalyzer(QWidget):
         # set the layout for the widget
         self.setLayout(layout)
 
+    def validate_ticker(self, ticker):
+        try:
+            stock = yf.Ticker(ticker)
+            info = stock.info
+            return True
+        except:
+            return False
+
     def get_basic_info(self):
         ticker = self.ticker_edit.text()
+        if not self.validate_ticker(ticker):
+            self.result_label.setText("Invalid ticker symbol. Please enter a valid ticker symbol.")
+            return
         try:
             stock = yf.Ticker(ticker)
             info = stock.info
@@ -58,10 +70,13 @@ class StockAnalyzer(QWidget):
                              f"52-Week Low: {info['fiftyTwoWeekLow']:.2f}"
             self.result_label.setText(basic_info_str)
         except:
-            self.result_label.setText("No Data")
+            self.result_label.setText("Data not available for this stock.")
 
     def get_historical_data(self):
         ticker = self.ticker_edit.text()
+        if not self.validate_ticker(ticker):
+            self.result_label.setText("Invalid ticker symbol. Please enter a valid ticker symbol.")
+            return
         try:
             stock = yf.Ticker(ticker)
             hist_data = stock.history(period='max')
@@ -74,10 +89,13 @@ class StockAnalyzer(QWidget):
             ax.set_title(f'{ticker} Historical Data')
             plt.show()
         except:
-            self.result_label.setText("No Data")
+            self.result_label.setText("Data not available for this stock.")
 
     def get_risk_return(self):
         ticker = self.ticker_edit.text()
+        if not self.validate_ticker(ticker):
+            self.result_label.setText("Invalid ticker symbol. Please enter a valid ticker symbol.")
+            return
         try:
             stock = yf.Ticker(ticker)
             hist_data = stock.history(period='max')
@@ -93,20 +111,26 @@ class StockAnalyzer(QWidget):
             ax.set_title(f'{ticker} Risk and Return')
             plt.show()
         except:
-            self.result_label.setText("No Data")
+            self.result_label.setText("Data not available for this stock.")
 
     def get_dividend_yield(self):
         ticker = self.ticker_edit.text()
+        if not self.validate_ticker(ticker):
+            self.result_label.setText("Invalid ticker symbol. Please enter a valid ticker symbol.")
+            return
         try:
             stock = yf.Ticker(ticker)
             info = stock.info
             dividend_yield = info['dividendYield']
             self.result_label.setText(f"Dividend Yield: {dividend_yield:.4f}")
         except:
-            self.result_label.setText("No Data")
+            self.result_label.setText("Data not available for this stock.")
 
     def get_technical_indicators(self):
         ticker = self.ticker_edit.text()
+        if not self.validate_ticker(ticker):
+            self.result_label.setText("Invalid ticker symbol. Please enter a valid ticker symbol.")
+            return
         try:
             stock = yf.Ticker(ticker)
             hist_data = stock.history(period='max')
@@ -122,8 +146,18 @@ class StockAnalyzer(QWidget):
             ax.set_title(f'{ticker} Technical Indicators')
             plt.show()
         except:
-            self.result_label.setText("No Data")
+            self.result_label.setText("Data not available for this stock.")
 
+class AnalysisThread(QThread):
+    analysis_complete = pyqtSignal(str)
+
+    def __init__(self, func):
+        super().__init__()
+        self.func = func
+
+    def run(self):
+        self.func()
+        self.analysis_complete.emit("Analysis complete.")
 
 if __name__ == '__main__':
     # create the QApplication
